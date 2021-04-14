8760b70b7b0432282a079978479975d46b47ec24
HusseinMalingha <husseinmalingha@gmail.com>
2021-04-14 20:33:56 +0300

    Added an apple, added collision effect and score of the game

--- main.py
+++ main.py
@@ -9,14 +9,38 @@ Created on Tue Apr 13 17:43:37 2021
 import pygame
 from pygame.locals import *
 import time
+from random import randint
 
-class Snake:
+SIZE = 40
+
+class Apple:
     def __init__(self, parent_screen):
         self.parent_screen = parent_screen
+        self.image = pygame.image.load("resources/apple.jpg").convert()
+        self.x = SIZE*3
+        self.y = SIZE*3
+
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+    def move(self):
+        self.x = randint(0,20)*SIZE
+        self.y = randint(0,20)*SIZE
+
+class Snake:
+    def __init__(self, surface, length):
+        self.length = length
+        self.parent_screen = surface
         self.block = pygame.image.load("resources/block.jpg").convert()
-        self.x = 100
-        self.y = 100
+        self.x = [SIZE]*length
+        self.y = [SIZE]*length
         self.direction = 'right'
+
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
 
     def move_up(self):
         self.direction = 'up'
@@ -31,28 +55,57 @@ class Snake:
         self.direction = 'right'
 
     def draw(self):
-        self.parent_screen.fill((0,0,50))
-        self.parent_screen.blit(self.block, (self.x, self.y))
+        self.parent_screen.fill((108,166,74))
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
 
     def walk(self):
+        for i in range(self.length-1, 0, -1):
+            self.x[i] = self.x[i-1]
+            self.y[i] = self.y[i-1]
+
         if self.direction == 'up':
-            self.y -= 10
+            self.y[0] -= SIZE
         if self.direction == 'down':
-            self.y += 10
+            self.y[0] += SIZE
         if self.direction == 'left':
-            self.x -= 10
+            self.x[0] -= SIZE
         if self.direction == 'right':
-            self.x += 10
+            self.x[0] += SIZE
         self.draw()
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((500,500))
+        self.surface = pygame.display.set_mode((1000,800))
         self.surface.fill((255,255,255))
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface,1)
         self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 +SIZE:
+                return True
+
+        return False
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+
+    def display_score(self):
+        font = pygame.font.SysFont('arial',30)
+        score = font.render(f"Score: {self.snake.length}", True, (255,255,255))
+        self.surface.blit(score, (800,10))
 
     def run(self):
         running = True
@@ -78,7 +131,8 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
-            self.snake.walk()
+            self.play()
+
             time.sleep(0.2)
 
 
